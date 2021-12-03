1f07981e17f181d3276a9d818a1e0a0d4c9aac2c
h3samayoa <h3samayoa@gmail.com>
2021-12-03 13:18:20 -0800

    added switch statement for choices

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ connection.connect((err) => {
     console.log(chalk.greenBright.bold(fig.textSync('Employee CMS Tracker')));
     console.log('');
     console.log(chalk.yellow.bold(`====================================================================================`));
+    prompt();
 })
 
 const prompt = () => {
@@ -20,12 +21,69 @@ const prompt = () => {
            type: 'list',
            message: 'Please Select an Option:',
            choices: [
-               'View all employees',
-               'View all roles',
-               'View all departments',
-               'View all employees by department',
-               
+            'View all employees',
+            'View all roles',
+            'View all departments',
+            'View all employees by department',
+            'View role salary',
+            'Update employee role',
+            'Update employee manager',
+            'Add employee',
+            'Add role',
+            'Add department',
+            'Remove employee',
+            'Remove role',
+            'Remove department',
+            'Quit'
            ]
         }
     ])
+    .then((ans) => {
+        const {choices} = ans;
+
+        switch(choices) {
+            case 'View all employees':
+                viewAllEmployees();
+                break;
+            case 'View all roles':
+                viewAllRoles();
+                break;
+            case 'View all departments':
+                viewAllDepartments();
+                break;
+            case 'View all employees by department':
+                viewAllEmployeesByDep();
+                break;
+            case 'View role salary':
+                viewRoleSalary();
+                break;
+            case 'Update employee role':
+                updateEmployeeRole();
+                break;
+            case 'Update employee manager':
+                updateEmployeeManager();
+                break;
+            case 'Add employee':
+                addEmployee();
+                break;
+            case 'Add role':
+                addRole();
+                break;
+            case 'Add department':
+                addDepartment();
+                break;
+            case 'Remove employee':
+                remEmployee();
+                break;
+            case 'Remove role':
+                remRole();
+                break;
+            case 'Remove department':
+                remDepartment();
+                break;
+            case 'Quit':
+                connection.end();
+                break;
+        }
+    })
 }
