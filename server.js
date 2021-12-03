ed5756516a05d2985735b576dd09b6cd581cbe31
h3samayoa <h3samayoa@gmail.com>
2021-12-03 11:50:59 -0800

    added figlet library for ascii art

---
+++ server.js
@@ -0,0 +1,12 @@
+const connection = require('./config/connection');
+const inquirer = require('inquirer');
+const cTab = require('console.table');
+const chalk = require('chalk');
+const fig = require('figlet')
+
+connection.connect((err) => {
+    if(err) throw err;
+    console.log(chalk.yellow.bold(`====================================================================================`));
+    console.log('');
+    console.log(chalk.greenBright.bold())
+})
